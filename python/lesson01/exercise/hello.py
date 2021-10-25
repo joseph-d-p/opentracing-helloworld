32e10ae99ed77960e6b172cffcc0c3fd0ef55473
joseph-d-p <josephandrewdelapena@gmail.com>
2021-10-25 22:30:21 +0800

    fix: python/lesson01 - Add jaeger client

--- python/lesson01/exercise/hello.py
+++ python/lesson01/exercise/hello.py
@@ -1,7 +1,28 @@
-import opentracing
 import sys
+import logging
+from jaeger_client import Config
 
-tracer = opentracing.tracer
+
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+
+tracer = init_tracer('hello')
 
 
 def say_hello(hello_to):
