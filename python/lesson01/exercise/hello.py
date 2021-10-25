2f9b46a59273d5caca9fe872577821a2ad748865
joseph-d-p <josephandrewdelapena@gmail.com>
2021-10-25 22:20:15 +0800

    python/lesson1: Add initial tracing

--- python/lesson01/exercise/hello.py
+++ python/lesson01/exercise/hello.py
@@ -1,8 +1,15 @@
+import opentracing
 import sys
 
+tracer = opentracing.tracer
+
+
 def say_hello(hello_to):
+    span = tracer.start_span('say-hello')
     hello_str = 'Hello, %s!' % hello_to
     print(hello_str)
+    span.finish()
+
 
 assert len(sys.argv) == 2
 
